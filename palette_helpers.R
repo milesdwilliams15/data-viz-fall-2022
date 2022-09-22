6a0db0dcac27218ec1a0e6993079f4bfb05f099a
Miles Williams <milesdwilliams15@gmail.com>
2022-09-21 20:27:45 -0400

    Update palette_helpers.R

--- palette_helpers.R
+++ palette_helpers.R
@@ -8,6 +8,35 @@ coolors <- function(url) {
   paste0("#", col)
 } 
 
+# palette setter function
+set_palette <- function(
+    qualitative = NULL,
+    diverging = NULL,
+    sequential = NULL,
+    binary = NULL
+    ) {
+  if(is.null(qualitative)) {
+    qual <<- coolors("https://coolors.co/265a73-e8c547-b0bbbf-a85751-331832")
+  } else {
+    qual <<- coolors(qualitative)
+  }
+  if(is.null(diverging)) {
+    dive <<- coolors("https://coolors.co/265a73-eff1f3-df2935")
+  } else {
+    dive <<- coolors(diverging)
+  }
+  if(is.null(sequential)) {
+    sequ <<- coolors("https://coolors.co/eff1f3-265a73")
+  } else {
+    sequ <<- coolors(sequential)
+  }
+  if(is.null(binary)) {
+    dual <<- coolors("https://coolors.co/faa916-265a73")
+  } else {
+    dual <<- coolors(binary)
+  }
+}
+
 # write a function to add palettes to ggplot
 ggpal <- function(type = "qualitative", aes = "color", midpoint = 0) {
   if(!(type %in% c("qualitative", "diverging", "sequential", "binary"))) {
